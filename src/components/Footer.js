3df750136bed1b823ae128f8a1be6fa9eff5680d
Nuno Marques <ositaka@gmail.com>
2020-03-16 15:19:14 +0000

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,47 +1,41 @@
-import React, { Component } from "react";
-import { Link } from "gatsby";
-import netlify from "../../content/images/css.png";
+import React, { Component } from 'react'
+import { Link } from 'gatsby'
+import netlify from '../../content/images/css.png'
 
 export default class Footer extends Component {
-  render() {
-    return (
-      <footer className="footer container">
-        <div>
-          <a
-            href="https://ko-fi.com/taniarascia"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Ko-Fi
-          </a>
-          <a
-            href="https://patreon.com/taniarascia"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Patreon
-          </a>
-          <Link to="/contact">Newsletter</Link>
-          <a
-            href="https://www.taniarascia.com/rss.xml"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            RSS
-          </a>
-        </div>
-        <div>
-          <a href="https://www.netlify.com/" title="Hosted by Netlify">
-            <img
-              src={netlify}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="footer-img"
-              alt="GitHub"
-            />
-          </a>
-        </div>
-      </footer>
-    );
-  }
+	render() {
+		return (
+			<footer className="footer container">
+				<div>
+					<a
+						href="https://mediacreators.studio"
+						title="Media Creators Studio | Design, Web, Online advertising"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						mediacreators.studio
+					</a>
+					<Link to="/contact">Contact </Link>
+					<a
+						href="https://www.taniarascia.com/rss.xml"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						RSS
+					</a>
+				</div>
+				<div>
+					<a href="https://www.netlify.com/" title="Hosted by Netlify">
+						<img
+							src={netlify}
+							target="_blank"
+							rel="noopener noreferrer"
+							className="footer-img"
+							alt="GitHub"
+						/>
+					</a>
+				</div>
+			</footer>
+		)
+	}
 }
